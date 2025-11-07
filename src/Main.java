7421c19d0f1ae62bcb82484a394da0978388568d
Patric <pmorevs@gmail.com>
2025-11-07 13:23:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String ... args){
+        List<String> myList = new ArrayList<String>();
+        myList.add("One");
+        myList.add("Two");
+        myList.add("Three");
+        myList.add("Four");
+        myList.add("Five");
+        myList.add("Six");
+        myList.add("Seven");
+        myList.add("Eight");
+        myList.add("Nine");
+        myList.add("Ten");
+        System.out.println(myList);
+        Stream myStream = myList.stream();
+        System.out.println(myStream);
+        myStream.forEach(System.out::print);
+    }
+}
